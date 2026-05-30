044738a6487b9869aafb1785ceae73a6906ff0aa
LiVSi <vadym.shylov@gmail.com>
2026-05-30 01:17:21 +0100

    refactor: replace os.path with pathlib

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import hashlib
-import os
 import json
 import datetime
 import shutil
@@ -7,22 +6,15 @@ from pathlib import Path
 
 
 # TODO: remove hardcode
-target_dir = r'C:\Users\user\py\tst_b_up\t1'
-target_dir_name = target_dir.split('\\')
-root_dir = os.path.dirname(target_dir)
-b_up_dir = root_dir + '\\' + target_dir_name[-1] + '_b_up'
+TARGET_DIR = Path(r'C:\Users\user\py\tst_b_up\t1')
+ROOT_DIR = Path(TARGET_DIR).parent
+B_UP_DIR = ROOT_DIR / f'{TARGET_DIR.name}_backup'
 
 current_datetime = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
 
-list_of_files = []
-dict_of_files_and_hashes = {}
-
 
 def get_list_of_files(dir_path):
-    for path, subdirs, files in os.walk(dir_path):
-        for name in files:
-            filename = os.path.join(path, name)
-            list_of_files.append(filename)
+    return [str(p) for p in Path(dir_path).rglob("*") if p.is_file()]
 
 
 def compute_file_hash(file_path, algorithm='sha256'):
@@ -36,22 +28,22 @@ def compute_file_hash(file_path, algorithm='sha256'):
     return hash_func.hexdigest()
 
 
-def get_dict_of_files_and_hashes():
-    for filename in list_of_files:
-        dict_of_files_and_hashes[filename] = compute_file_hash(filename)
+def get_dict_of_files_and_hashes(list_of_files):
+    return {filepath: compute_file_hash(filepath) for filepath in list_of_files}
 
 
-def save_json():
-    with open(b_up_dir + r'\snapshot_last.json', 'w') as f:
+def save_json(dict_of_files_and_hashes):
+    with (B_UP_DIR / 'snapshot_last.json').open('w') as f:
         json.dump(dict_of_files_and_hashes, f, indent=4)
-    with open(b_up_dir + r'\snapshot_' + current_datetime + '.json', 'w') as f:
+    with (B_UP_DIR / ('snapshot_' + current_datetime + '.json')).open('w') as f:
         json.dump(dict_of_files_and_hashes, f, indent=4)
 
 
-def backup_changed_files():
-    os.makedirs(b_up_dir, exist_ok=True)
+def backup_changed_files(dict_of_files_and_hashes):
+    B_UP_DIR.mkdir(parents=True, exist_ok=True)
+    snapshot_path = B_UP_DIR / 'snapshot_last.json'
     try:
-        with open(b_up_dir + r'\snapshot_last.json') as json_file:
+        with snapshot_path.open() as json_file:
             prev_snapshot = json.load(json_file)
     except FileNotFoundError:
         prev_snapshot = {}
@@ -61,16 +53,21 @@ def backup_changed_files():
             create_file_b_up(k)
 
 
+# TODO: handle double suffixes
 def create_file_b_up(src):
-    dst = Path(b_up_dir + '\\files\\' + src.split(target_dir)[-1])
+    src = Path(src)
+    rel_path = src.relative_to(TARGET_DIR)
+    dst = B_UP_DIR / 'files' / rel_path
     dst.parent.mkdir(parents=True, exist_ok=True)
-    name, ext = os.path.splitext(dst)
-    dst = "{name}_{timestamp}{ext}".format(name=name, timestamp=current_datetime, ext=ext)
+
+    name = dst.stem
+    ext = dst.suffix
+    dst = dst.with_name(f"{name}_{current_datetime}{ext}")
     shutil.copyfile(src, dst)
 
 
-get_list_of_files(target_dir)
-get_dict_of_files_and_hashes()
-backup_changed_files()
-save_json()
+list_of_files = get_list_of_files(TARGET_DIR)
+dict_of_files_and_hashes = get_dict_of_files_and_hashes(list_of_files)
+backup_changed_files(dict_of_files_and_hashes)
+save_json(dict_of_files_and_hashes)
 
